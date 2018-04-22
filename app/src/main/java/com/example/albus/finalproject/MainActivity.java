3fcffce8dffa7f7bc753a3bc31a717945a424ca3
albus <df4@illinois.edu>
2018-04-22 16:02:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/albus/finalproject/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.albus.finalproject;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+            Request.Method.GET,
+            "",
+            null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(final JSONObject response) {
+                    Log.d(TAG, response.toString());
+                }
+            }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(final VolleyError error) {
+            Log.w(TAG, error.toString());
+        }
+    });*/
+}
