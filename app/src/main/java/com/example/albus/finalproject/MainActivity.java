e0a18bce3a0106b78e53467ed511dd7d6f5ea9c0
albus <df4@illinois.edu>
2018-04-22 16:17:15 -0500

    another commit

--- app/src/main/java/com/example/albus/finalproject/MainActivity.java
+++ app/src/main/java/com/example/albus/finalproject/MainActivity.java
@@ -5,10 +5,10 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-import org.json.JSONObject;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
