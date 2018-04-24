354717ec50b238a7a78cd6ed3db74c8653d15fa7
aruizgehrt <ruizgeh2@illinois.edu>
2018-04-24 15:41:52 -0500

    Trying to address errors in API call.

--- app/src/main/java/com/example/albus/finalproject/MainActivity.java
+++ app/src/main/java/com/example/albus/finalproject/MainActivity.java
@@ -28,8 +28,7 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static RequestQueue requestQueue;
-    public static String category = "none";
-
+    private static String category = "none";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
+                Log.d(TAG, "Nothing selected");
             }
         });
 
@@ -60,81 +59,108 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {
                 Log.d(TAG, "Make Call");
+                makeAPICall(category);
                 setContentView(R.layout.question_page);
-  //              makeAPICall("none");
 
             }
 
         });
+
+
+
     }
 
 
-  //  public static void makeAPICall(String category) {
-  //      if (category.equals("none")) {
-//            try {
-  //              JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-    //                    Request.Method.GET,
-    //                    "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&encode=url3986",
-    //                    null,
-    //                    new Response.Listener<JSONObject>() {
-    //                        @Override
-    //                        public void onResponse(final JSONObject response) {
-    //                           Log.d(TAG, "Response Received");
-     //                          String[] questions = getQuestion(response);
-    //                           makeQuestionPage(questions);
-    //                        }
-    //                    }, new Response.ErrorListener() {
-    //                @Override
-   //                 public void onErrorResponse(final VolleyError error) {
-    //                    Log.w(TAG, error.toString());
-    //                }
-    //            });
-    //            requestQueue.add(jsonObjectRequest);
-    //        } catch (Exception e) {
-    //            e.printStackTrace();
-    //        }
-    //    } else {
-    //        try {
-    //            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-    //                    Request.Method.GET,
-    //                    "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&encode=url3986&" + category,
-    //                    null,
-    //                    new Response.Listener<JSONObject>() {
-    //                        @Override
-    //                        public void onResponse(final JSONObject response) {
-    //                            Log.d(TAG, response.toString());
-     //                       }
-      //                  }, new Response.ErrorListener() {
-      //              @Override
-       //             public void onErrorResponse(final VolleyError error) {
-       //                 Log.w(TAG, error.toString());
-       //             }
-        //        });
-         //       requestQueue.add(jsonObjectRequest);
-          //  } catch (Exception e) {
-           //     e.printStackTrace();
-      //      }
-      //  }
-  //  }
-///*
-//Log.d(TAG, response.toString());
-  //      String[] questions = new String[10];
-  //      try {
-  //          JSONArray results = response.getJSONArray("results");
-  //          for (int i = 0; i < results.length(); i++) {
-  //              JSONObject question = results.getJSONObject(i);
-  //              String question1 = question.get("question").toString();
-  //              questions[i] = URLDecoder.decode(question1);
-  //          }
-  //      } catch (Exception e) {
- //           e.printStackTrace();
- //       }
- //       //Log.d(TAG, questions[1]);
- //       return questions;
-// */
-    //private static JSONObject getQuestion(JSONObject response) { }
-
-    //private static void makeQuestionPage(final String[] questions, final String[] answers) { }
+    /**
+     * makes an api call, triggering the beginning of the game.
+     *
+     * @param category sets the category of questions
+     */
+    public static void makeAPICall(String category) {
+        if (category.equals("none")) {
+            try {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.GET,
+                        "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&encode=url3986",
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(final JSONObject response) {
+                                Log.d(TAG, "Request Received: " + response.toString());
+                                Question[] questions = getQuestions(response);
+                                //makeQuestionPage(questions);
 
 
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(final VolleyError error) {
+                        Log.w(TAG, error.toString());
+                    }
+                });
+                requestQueue.add(jsonObjectRequest);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.GET,
+                        "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&encode=url3986&" + category,
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(final JSONObject response) {
+                                Log.d(TAG, response.toString());
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(final VolleyError error) {
+                        Log.w(TAG, error.toString());
+                    }
+                });
+                requestQueue.add(jsonObjectRequest);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * reads and parses the return json, creating question objects for all received questions
+     *
+     * @param response the json received from api call
+     * @return an array of question objects
+     */
+    private static Question[] getQuestions(JSONObject response) {
+        Question[] questions = new Question[10];
+        try {
+            JSONArray result = response.getJSONArray("results");
+            for (int index = 0; index < questions.length; index++) {
+                String q = result.getJSONObject(index).get("question").toString();
+                q = URLDecoder.decode(q);
+                String correct_answer = result.getJSONObject(index).get("correct_answer").toString();
+                String[] incorrect_answers = new String[3];
+                for (int j = 0; j < incorrect_answers.length; j++) {
+                    incorrect_answers[j] = result.getJSONObject(index).getJSONArray("incorrect_answers").get(j).toString();
+                }
+                questions[index] = new Question(q, correct_answer, incorrect_answers);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return questions;
+    }
+
+
+    /**
+     * tbd.
+     *
+     * @param questions an array of question objects
+     */
+    private static void makeQuestionPage(final Question[] questions) {
+
+
+    }
 }
