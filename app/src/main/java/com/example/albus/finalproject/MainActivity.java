91ebefe1f6fa9c027548c71c9b3052ac768c0d26
kevinterwilliger <kevint3@illinois.edu>
2018-04-23 21:00:13 -0500

    process JSON

--- app/src/main/java/com/example/albus/finalproject/MainActivity.java
+++ app/src/main/java/com/example/albus/finalproject/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import java.net.URLDecoder;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -36,24 +37,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {
                 Log.d(TAG, "Make Call");
-                startGame("none");
+                makeAPICall("none");
 
             }
 
         });
     }
 
-    /*void makeAPICall(String category) {
+    public static void makeAPICall(String category) {
         if (category.equals("none")) {
             try {
                 JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                         Request.Method.GET,
-                        "https://opentdb.com/api.php?amount=10",
+                        "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&encode=url3986",
                         null,
                         new Response.Listener<JSONObject>() {
                             @Override
                             public void onResponse(final JSONObject response) {
-                                Log.d(TAG, response.toString());
+                               Log.d(TAG, "Response Received");
+                               String[] questions = getQuestion(response);
+                               makeQuestionPage(questions);
                             }
                         }, new Response.ErrorListener() {
                     @Override
@@ -69,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                         Request.Method.GET,
-                        "https://opentdb.com/api.php?amount=10&" + category,
+                        "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&encode=url3986&" + category,
                         null,
                         new Response.Listener<JSONObject>() {
                             @Override
@@ -87,73 +90,28 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-    } */
+    }
+/*
+//Log.d(TAG, response.toString());
+        String[] questions = new String[10];
+        try {
+            JSONArray results = response.getJSONArray("results");
+            for (int i = 0; i < results.length(); i++) {
+                JSONObject question = results.getJSONObject(i);
+                String question1 = question.get("question").toString();
+                questions[i] = URLDecoder.decode(question1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Log.d(TAG, questions[1]);
+        return questions;
+ */
+    private static JSONObject getQuestion(JSONObject response) {
 
-    private static void startGame(String category) {
-        if (category.equals("none")) {
-            try {
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                        Request.Method.GET,
-                        "https://opentdb.com/api.php?amount=10",
-                        null,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(final JSONObject response) {
-                                Log.d(TAG, response.toString());
-                                String[] questions = new String[10];
-                                try {
-                                    JSONArray results = response.getJSONArray("results");
-                                    for (int i = 0; i < results.length(); i++) {
-                                        JSONObject question = results.getJSONObject(i);
-                                        questions[i] = question.get("question").toString();
-                                    }
-                                } catch (JSONException e) {
-                                      Log.e(TAG, "i fucked up");
-                                }
-                                Log.d(TAG, questions[0]);
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(final VolleyError error) {
-                        Log.w(TAG, error.toString());
-                    }
-                });
-                requestQueue.add(jsonObjectRequest);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            try {
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                        Request.Method.GET,
-                        "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&" + category,
-                        null,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(final JSONObject response) {
-                                Log.d(TAG, response.toString());
-                                String[] questions = new String[10];
-                                try {
-                                    JSONArray results = response.getJSONArray("results");
-                                    for (int i = 0; i < results.length(); i++) {
-                                        JSONObject question = results.getJSONObject(i);
-                                        questions[i] = question.get("question").toString();
-                                    }
-                                } catch (JSONException e) {
-                                    Log.e(TAG, "i fucked up");
-                                }
-                                Log.d(TAG, questions[0]);
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(final VolleyError error) {
-                        Log.w(TAG, error.toString());
-                    }
-                });
-                requestQueue.add(jsonObjectRequest);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+    }
+
+    private static void makeQuestionPage(final String[] questions, final String[] answers) {
+
     }
 }
