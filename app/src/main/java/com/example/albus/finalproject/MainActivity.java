cc5f50ad49e3d4cd6d0ffc99c90c446bcd8eede3
kevinterwilliger <kevint3@illinois.edu>
2018-04-25 20:36:05 -0500

    categories fixed

--- app/src/main/java/com/example/albus/finalproject/MainActivity.java
+++ app/src/main/java/com/example/albus/finalproject/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import java.net.URLDecoder;
-import java.util.Random;
+import java.util.Hashtable;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -29,11 +29,13 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static RequestQueue requestQueue;
-    private static String category = "none";
+    private static Hashtable<String,Integer> categories = new Hashtable<>(); //this allows us to keep the categories and their ids in one place yay
+    private static int category; //we need the category to be the matching id number
     private static Question[] questions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        buildMap();
         super.onCreate(savedInstanceState);
         requestQueue = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_main);
@@ -46,8 +48,16 @@ public class MainActivity extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                category = parent.getItemAtPosition(position).toString();
-                Log.d(TAG, "Category set to " + category);
+                try {
+                    String temp = parent.getItemAtPosition(position).toString(); //gets string from spinner
+                    if (temp.equalsIgnoreCase("Choose a category") || temp.equalsIgnoreCase("miscellaneous")) {
+                        temp = "General Knowledge"; //this is the key for the random category
+                    }
+                    category = categories.get(temp); //get id number from hashtable
+                    Log.d(TAG, "Category set to " + category);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
@@ -61,82 +71,47 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {
                 Log.d(TAG, "Make Call");
-                makeAPICall(category);
+                makeAPICall();
             }
         });
     }
 
     /**
      * makes an api call, triggering the beginning of the game.
-     * @param category sets the category of questions
+     *
      */
-    public void makeAPICall(String category) {
-        if (category.equals("none") || category.equalsIgnoreCase("Choose your Category")) {
+    public void makeAPICall() {
             try {
                 JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                         Request.Method.GET,
-                        "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&encode=url3986",
+                        "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&encode=url3986&category=" + category,
                         null,
                         new Response.Listener<JSONObject>() {
                             @Override
                             public void onResponse(final JSONObject response) {
                                Log.d(TAG, "Request Received:"+ response.toString());
+
                                questions = getQuestions(response);
-                               //Log.d(TAG, questions[0].getQuestion());
+
                                setContentView(R.layout.question_page);
+
                                final TextView question = findViewById(R.id.question_view);
                                question.setText(questions[0].getQuestion());
-                               Log.d(TAG, questions[0].getAnswers().toString());
+
                                String[] answers = questions[0].shuffleAnswers();
+
                                final Button answer1 = findViewById(R.id.answer_1);
                                answer1.setText(answers[0]);
+
                                final Button answer2 = findViewById(R.id.answer_2);
                                answer2.setText(answers[1]);
+
                                final Button answer3 = findViewById(R.id.answer_3);
                                answer3.setText(answers[2]);
+
                                final Button answer4 = findViewById(R.id.answer_4);
                                answer4.setText(answers[3]);
 
-
-
-
-
-                               //Log.d(TAG, "Random: " + num);
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(final VolleyError error) {
-                        Log.w(TAG, error.toString());
-                    }
-                });
-                requestQueue.add(jsonObjectRequest);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            try {
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                        Request.Method.GET,
-                        "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&encode=url3986&" + category,
-                        null,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(final JSONObject response) {
-                                Log.d(TAG, "Response Received: " + response.toString());
-                                questions = getQuestions(response);
-                                setContentView(R.layout.question_page);
-                                final TextView question = findViewById(R.id.question_view);
-                                question.setText(questions[0].getQuestion());
-                                //Log.d(TAG, questions[0].getAnswers().toString());
-                                String[] answers = questions[0].shuffleAnswers();
-                                final Button answer1 = findViewById(R.id.answer_1);
-                                answer1.setText(answers[0]);
-                                final Button answer2 = findViewById(R.id.answer_2);
-                                answer2.setText(answers[1]);
-                                final Button answer3 = findViewById(R.id.answer_3);
-                                answer3.setText(answers[2]);
-                                final Button answer4 = findViewById(R.id.answer_4);
-                                answer4.setText(answers[3]);
                             }
                         }, new Response.ErrorListener() {
                     @Override
@@ -149,7 +124,6 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-    }
     /**
      * reads and parses the return json, creating question objects for all received questions
      * @param response the json received from api call
@@ -176,5 +150,17 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return questions;
+    }
+
+    /**
+     * i didn't know how to do this any other way. There is probably a better way to initialize the map but idk.
+     */
+    private static void buildMap() {
+        categories.put("General Knowledge",9);
+        categories.put("Entertainment: Books",11);
+        categories.put("Entertainment: Music",12);
+        categories.put("Science: Computer",18);
+        categories.put("Science: Math",19);
+        categories.put("Vehicle",28);
     }
 }
